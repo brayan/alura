b9994125175e27f7f02bb3ea932d5e631ee2a510
Brayan Bedritchuk <brayan.bedritchuk@gmail.com>
2018-01-25 10:09:36 -0200

    Add front-end content

---
+++ front-end/bootstrap/projeto_arquitetura/assets/js/navbar-animation-fix.js
@@ -0,0 +1,11 @@
+$('#collapse-navbar').on('show.bs.collapse', function () {
+
+	$('.topCasaFina-banner').css('transform', 'translate(-40%, 10%');
+
+});
+
+$('#collapse-navbar').on('hide.bs.collapse', function () {
+
+	$('.topCasaFina-banner').css('transform', 'translate(-40%, -40%');
+	
+});
